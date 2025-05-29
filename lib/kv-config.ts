eba566a956410639409c0db536e7efb6421e288c
Lucas Valbuena <lucknitelol@proton.me>
2025-05-29 17:08:58 +0200

    Update kv-config.ts

--- lib/kv-config.ts
+++ lib/kv-config.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@vercel/kv"
 
 
-const KV_REST_API_URL = "https://pet-scorpion-19203.upstash.io"
-const KV_REST_API_TOKEN = "AUsDAAIjcDFiNzI1ZDEwMGU0YmM0NWIzYjlkNjgzN2RhOGU3YjQ2OXAxMA"
+const KV_REST_API_URL = process.env.KV_REST_API_URL
+const KV_REST_API_TOKEN = process.env.KV_REST_API_TOKEN
 
 
 export const kv = createClient({
